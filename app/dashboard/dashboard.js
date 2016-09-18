aaef9c17581f36ba1be028fa816146baf03defed
Yan Ke <yansongke1@gmail.com>
2016-09-18 01:59:22 -0400

    test

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -1,41 +1,22 @@
 'use strict';
 
-angular.module('myApp.dashboard', ['ngRoute', 'ngStorage']).config(['$routeProvider',
+var dashboardApp = angular.module('myApp.dashboard', ['ngRoute', 'ngStorage']).config(['$routeProvider',
 function($routeProvider) {
 	$routeProvider.when('/preview', {
 		templateUrl : 'dashboard/dashboard.html',
 		controller : 'dashboardCtrl'
 	});
-}]).controller('dashboardCtrl', ['$rootScope', '$scope', '$http', '$localStorage', '$timeout', '$interval', '$sce', '$firebaseObject', '$firebaseArray', '$firebaseAuth',
+}]);
+
+dashboardApp.controller('dashboardCtrl', ['$rootScope', '$scope', '$http', '$localStorage', '$timeout', '$interval', '$sce', '$firebaseObject', '$firebaseArray', '$firebaseAuth',
 function($rootScope, $scope, $http, $localStorage, $timeout, $interval, $sce, $firebaseObject, $firebaseArray, $firebaseAuth) {
-/*
+	$scope.dashSearch = "";
+
 	$scope.exportData() = function(){
-		var exportee = new Blob([document.getElementById('exportable').innerHTML], {
+		var blob = new Blob([document.getElementById('exportable').innerHTML], {
 			type: "application.vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
 		});
-		saveAs(exportee, "CandidateList.xls")
-	}
-
-/*
-	$scope.$storage = $localStorage;
-
-	var allRef = firebase.database().ref();
-	$scope.$storage.allRef = $firebaseArray(allRef);
-
-	var deals = firebase.database().ref().child("/deals/");
-	$scope.$storage.deals = $firebaseArray(deals);
-
-	var beacons = firebase.database().ref().child("/beacons/");
-	$scope.$storage.beacons = $firebaseArray(beacons);
-
-	var nutrition = firebase.database().ref().child("/nutrition/");
-	$scope.$storage.nutrition = $firebaseArray(nutrition);
-
-	$scope.addNutritionItem = function(itemToAdd) {
-		$scope.$storage.deals = itemToAdd;
-		addItem('nutrition', $scope.$storage.deals);
+		saveAs(blob, "CandidateList.xls");
 	};
-*/
-
 
 }])
